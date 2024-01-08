fcf437a55310beceb412db1d1417a643c5b453b3
yuval2313 <lyuval1210@gmail.com>
2024-01-08 20:51:08 +0200

    EKS - Added dependency on node group to add ons

--- modules/eks/cluster.tf
+++ modules/eks/cluster.tf
@@ -31,16 +31,24 @@ resource "aws_eks_node_group" "node_group" {
 resource "aws_eks_addon" "aws-ebs-csi-driver" {
   cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "aws-ebs-csi-driver"
+
+  depends_on = [aws_eks_node_group.node_group]
 }
 resource "aws_eks_addon" "kube-proxy" {
   cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "kube-proxy"
+
+  depends_on = [aws_eks_node_group.node_group]
 }
 resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "coredns"
+
+  depends_on = [aws_eks_node_group.node_group]
 }
 resource "aws_eks_addon" "vpc-cni" {
   cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "vpc-cni"
+
+  depends_on = [aws_eks_node_group.node_group]
 }
